3fbc3e14dfec026589d0e2533c7dccffc01d6458
Thanos Kostoulas <tgkostoulas@gmail.com>
2019-09-20 14:54:34 +0300

    Update custom.php

--- custom.php
+++ custom.php
@@ -287,3 +287,47 @@ $argsynenteykseis = array(
 $loopargs = new WP_Query( $args );
 
 
+// SEND SUBSCRIBER DATA TO MAILCHIMP VIA API
+
+<?php
+$data = [
+    'email'     => 'customer-email',
+    'status'    => 'subscribed',
+    'firstname' => 'customer-firstname',
+    'lastname'  => 'customer-lastname'
+];
+
+syncMailchimp($data);
+
+function syncMailchimp($data) {
+    $apiKey = 'API-KEY';
+    $listId = 'LIST-ID';
+
+    $memberId = md5(strtolower($data['email']));
+    $dataCenter = substr($apiKey,strpos($apiKey,'-')+1);
+    $url = 'https://' . $dataCenter . '.api.mailchimp.com/3.0/lists/' . $listId . '/members/' . $memberId;
+
+    $json = json_encode([
+        'email_address' => $data['email'],
+        'status'        => $data['status'],
+        'merge_fields'  => [
+            'FNAME'     => $data['firstname'],
+            'LNAME'     => $data['lastname']
+        ]
+    ]);
+
+    $ch = curl_init($url);
+
+    curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $apiKey);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json);                                                                                                                 
+    $result = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    return $httpCode;
+}
